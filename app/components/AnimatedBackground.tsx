934c2466151bb9b06818f0a6ff05bfe36b39dcb4
mdsiamss10 <mdsiamss10@gmail.com>
2023-11-14 10:17:26 +0600

    Removing window object test-1

--- app/components/AnimatedBackground.tsx
+++ app/components/AnimatedBackground.tsx
@@ -1,38 +1,7 @@
-"use client";
-
-import { useEffect, useState } from "react";
-
 function AnimatedBackground() {
-  const [containerHeight, setContainerHeight] = useState<any>(
-    window.visualViewport?.height || window.innerHeight
-  );
-  //! Change viewport height
-  useEffect(() => {
-    const updateHeight = () => {
-      const newHeight = window.visualViewport?.height || window.innerHeight;
-      setContainerHeight(newHeight);
-
-      // Change body's maximum height
-      document.body.style.maxHeight = `${newHeight}px`;
-    };
-
-    window.addEventListener("resize", updateHeight);
-    window.addEventListener("orientationchange", updateHeight);
-
-    return () => {
-      window.removeEventListener("resize", updateHeight);
-      window.removeEventListener("orientationchange", updateHeight);
-    };
-  }, []);
   return (
     <>
-      <div
-        className="background"
-        style={{
-          height: containerHeight,
-          maxHeight: containerHeight,
-        }}
-      >
+      <div className="background">
         <span></span>
         <span></span>
         <span></span>
